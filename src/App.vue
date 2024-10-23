059fd563615a1ef67f665d33b2bfce496e029f77
Felipe Martins <felipecardoso1508@gmail.com>
2024-10-23 14:07:06 -0300

    diretiva e vmodel

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,97 @@
 <template>
-  <TheHeader />
+  <div>
+      <div>
+          One-way data binding <br>
+          Two-way data binding <br>
+          v-model -> formulários
+      </div>
 
-  <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-  >
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+      <br><br>
+
+      <div>
+          <label>Nome</label> <br>
+          <input
+              v-model="name"
+              type="text"
+          > <br>
+          {{ name }}
+      </div>
+
+      <br><br>
+
+      <div>
+          <label>Sports</label> <br>
+          <select v-model="sports">
+              <option value="">Escolha</option>
+              <option value="futebol">Futebol</option>
+              <option value="skate">Skate</option>
+              <option value="Tenis">Tenis</option>
+          </select> <br>
+          {{ sports }}
+      </div>
+
+      <br><br>
+
+      <div>
+          <label>Newsletter</label> <br>
+          <input
+              v-model="newsletter"
+              type="radio"
+              value="Sim"
+          > Sim
+
+          <input
+              v-model="newsletter"
+              type="radio"
+              value="Não"
+          > Não <br>
+          {{ newsletter }}
+      </div>
+
+      <br><br>
+
+      <div>
+          <label>Contrato</label> <br>
+          <input
+              v-model="contract"
+              type="checkbox"
+          > Aceita nosso termos... <br>
+
+            {{ contract }}
+      </div>
+
+      <br><br>
+
+      <div>
+          <label>Cores que você mais gosta</label> <br>
+          <input
+              v-model="colors"
+              type="checkbox"
+              value="Azul"
+          > Azul
+
+          <input
+              v-model="colors"
+              type="checkbox"
+              value="Amarelo"
+          > Amarelo <br>
+            {{ colors }}
+      </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader';
-
 export default {
   name: 'App',
-  components: {
-      HelloWorld,
-      TheHeader
-  },
+  data() {
+      return {
+          name: 'jon snow',
+          sports: 'futebol',
+          newsletter: 'Não',
+          contract: true,
+          colors: [],
+      }
+  }
 }
 </script>
 
@@ -26,8 +100,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
 </style>
