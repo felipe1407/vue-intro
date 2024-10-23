1726ee7e728c9fa3f55270af91d6769c429caa6d
Felipe Martins <felipecardoso1508@gmail.com>
2024-10-23 15:26:24 -0300

    Propriedades Computadas

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,84 @@
 <template>
-  <TheHeader />
+   
+      <div>
+        {{ fullName }} <br>
+      </div>
 
-  <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-  >
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+      <h2>Todos em aberto</h2>
+      
+      <div
+        v-for="todo in uncompletedTodos"
+        :key="todo.id"
+      >
+        {{ todo.title }} <span>{{ todo.completed }}</span>
+      </div>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader';
+
 
 export default {
   name: 'App',
-  components: {
-      HelloWorld,
-      TheHeader
+  data() {
+
+      return {
+          user: {
+              first_name: 'Jon',
+              last_name: 'Snow',
+
+      
   },
+  todos: [
+                {
+                    "userId": 1,
+                    "id": 1,
+                    "title": "delectus aut autem",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 2,
+                    "title": "quis ut nam facilis et officia qui",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 3,
+                    "title": "fugiat veniam minus",
+                    "completed": false
+                },
+                {
+                    "userId": 1,
+                    "id": 4,
+                    "title": "et porro tempora",
+                    "completed": true
+                },
+                {
+                    "userId": 1,
+                    "id": 5,
+                    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+                    "completed": false
+                }
+            ]
+        };
+      },
+
+    computed: {
+         fullName() {
+             return `${this.user.first_name} ${this.user.last_name}`
+        },
+
+        uncompletedTodos() {
+          return this.todos.filter(todo => !todo.completed);
+        },
+    },
+
+
+    methods: {
+     
+    }
+  
 }
 </script>
 
