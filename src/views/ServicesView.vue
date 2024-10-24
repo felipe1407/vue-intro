c07926b6dd32da91e4b2ab1d5b71535dbe73679f
Felipe Martins <felipecardoso1508@gmail.com>
2024-10-24 11:23:15 -0300

    adicionando routes

---
+++ src/views/ServicesView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        serviços
+    </div>
+   
+</template>
---
+++ src/views/UserEditView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      
+            {{  $route.query.page }}
+        
+        Usuário <br>
+
+        <button @click="login()">
+            Login
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        created() {
+            console.log(this.$router);
+        },
+
+        methods: {
+            login() {
+            this.$router.push('/about')
+            console.log('chegou');
+        }
+    }
+}
+</script>
