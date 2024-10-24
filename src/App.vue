280913d2d023cedb51de7b25a9c28a3249b52b2e
Felipe Martins <felipecardoso1508@gmail.com>
2024-10-24 16:59:21 -0300

    Nova main

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/usuarios/10">Usuarios</router-link> |
-    <router-link :to="rotaDinamica">Serviços</router-link>  |
-    <router-link to="/about">About</router-link> 
-  </nav>
-  <router-view/>
+
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-  export default {
-    data() {
-        return {
-            rotaDinamica: {name: 'servicos' }
-        }
-    },
-  }
+import HelloWorld from './components/HelloWorld.vue'
 
+
+export default {
+  name: 'App',
+  components: {
+      HelloWorld
+  },
+  
+}
 </script>
 
 <style>
@@ -25,18 +22,6 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
--- src/views/ServicesView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        serviços
-    </div>
-   
-</template>
